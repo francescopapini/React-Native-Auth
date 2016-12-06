bdc7fc552074b4e1de3489cbdf7c525a5e7143ab
francescopapini <francescopapini01@gmail.com>
2016-12-06 10:36:55 +0000

    setup firebase

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ small react-native app to test auth with Firebase.
 *********************************
 To install eslint pls follow these steps:
 
--npm install -g eslint
--npm install --save-dev eslint-config-rallycoding
+1) npm install -g eslint
+2) npm install --save-dev eslint-config-rallycoding
 
 if you are using SublimeText3, install the following packages:
-- SublimeLinter
-- SublimeLinter-conrtib-eslint
+3) SublimeLinter
+4) SublimeLinter-conrtib-eslint
+
+********************************
+to install firebase pls follow these steps: 
+
+1) npm install --save firebase 
+2) in the Firebase console, click on Web setup and copy paste the config object and pass it in the life cycle method defined in app.js
--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native'; 
+import firebase from 'firebase';
+import { Header } from './components/common/index.js';
+
+// i can skip specifying index.js when the file is called index
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB6eGJV6TbSDslEcULMwFo49kamcN-iNsM',
+      authDomain: 'auth-b292c.firebaseapp.com',
+      databaseURL: 'https://auth-b292c.firebaseio.com',
+      storageBucket: 'auth-b292c.appspot.com',
+      messagingSenderId: '69349472345'
+    });
+  }
+
   render() {
     return (
     <View>
+      <Header headerText="Authentication" />
       <Text>An App</Text>
     </View>
     );
